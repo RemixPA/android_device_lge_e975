8f1b2422d3bfa098dacc310899562524fa266a9b
xilence2210 <xilence2210@gmail.com>
2014-03-18 16:53:33 +0800

    Fix cmhw path
    Change-Id: I93ad9b3707af19116a2e6b3d39d3bdfa2ab353b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,4 +76,4 @@ BOARD_SEPOLICY_UNION := \
 ## We need this for FM support
 TARGET_QCOM_AUDIO_VARIANT := caf
 
-BOARD_HARDWARE_CLASS += device/lge/e975/cmhw
+BOARD_HARDWARE_CLASS += device/lge/geehrc/cmhw
